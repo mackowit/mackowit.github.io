59a689d9e8c337e44d9f81bda1cef27dba1927a5
mackowit <mackowit@o2.pl>
2020-02-25 09:42:20 +0100

    next update commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://rocky-chamber-85760.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://rocky-chamber-85760.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
